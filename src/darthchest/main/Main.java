23f6f8ad0d25818f53886c94afec6f3d17067723
Jonas Tenbrink <j_tenb03@web.de>
2017-03-30 21:59:37 +0200

    libraries

---
+++ src/darthchest/main/Main.java
@@ -0,0 +1,14 @@
+package darthchest.main;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	//RegisteredServiceProvider<Economy> reg = getServer().getServicesManager().getRegistration(Economy.class);
+
+
+	
+	
+	
+}
