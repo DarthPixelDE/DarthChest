fd226c36d87446ed8dcf486d2c88749232b90cd8
Jonas Tenbrink <j_tenb03@web.de>
2017-03-30 22:19:27 +0200

    Jars included

--- src/darthchest/main/Main.java
+++ src/darthchest/main/Main.java
@@ -3,9 +3,12 @@ package darthchest.main;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.milkbowl.vault.economy.Economy;
+
 public class Main extends JavaPlugin {
 	
-	//RegisteredServiceProvider<Economy> reg = getServer().getServicesManager().getRegistration(Economy.class);
+	
+	RegisteredServiceProvider<Economy> serv = getServer().getServicesManager().getRegistration(Economy.class);
 
 
 	
